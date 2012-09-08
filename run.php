aabfd7f8efcd8b8c047560fc9ff0a1e24c371025
Phil Lavin <phil@propcom.co.uk>
2012-09-08 21:15:31 +0100

    Remove debug

--- run.php
+++ run.php
@@ -28,7 +28,7 @@ while (1) {
 	if (($counter != 1 && $counter < 10) || ($counter % 10 == 0)) {
 		// Night
 		if ($timeline->is_greyscale()) {
-			$timeline->set_movement_threshold(1);
+			$timeline->set_movement_threshold(0.94);
 		}
 		// Day
 		else {
